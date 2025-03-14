55cb53147dc7ed2432328ec5d653f1da5424a645
Alek Evans <github@alekeagle.com>
2025-03-14 12:52:51 -0800

    fart the sequel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { ApplicationCommandTypes, Client } from 'oceanic.js';
+import {
+  ApplicationCommandOptionTypes,
+  ApplicationCommandTypes,
+  Client,
+  CreateApplicationCommandOptions,
+} from 'oceanic.js';
 import { config } from 'dotenv';
 import { exec } from 'node:child_process';
+import { writeFile, readFile, mkdir } from 'node:fs/promises';
 
 config();
 
@@ -11,23 +17,28 @@ const client = new Client({
   },
 });
 
+const TEST_SERVER = '456542159210807307';
+
+const commands: any = [
+  {
+    type: ApplicationCommandTypes.CHAT_INPUT,
+    name: 'orbify',
+    description: 'Turn your beloved image into an orb.',
+    options: [
+      {
+        type: ApplicationCommandOptionTypes.ATTACHMENT,
+        name: 'image',
+        description: 'The image to orbify.',
+        required: true,
+      },
+    ],
+  },
+];
+
 client.on('ready', async () => {
   console.log('Client is ready!');
-  await client.application.bulkEditGlobalCommands([
-    {
-      type: ApplicationCommandTypes.CHAT_INPUT,
-      name: 'orbify',
-      description: 'Turn your beloved image into an orb.',
-      options: [
-        {
-          type: 3,
-          name: 'image',
-          description: 'The image to orbify.',
-          required: true,
-        },
-      ],
-    },
-  ]);
+  await client.application.bulkEditGlobalCommands(commands);
+  await client.application.bulkEditGuildCommands(TEST_SERVER, commands);
 });
 
 client.on('interactionCreate', async (interaction) => {
@@ -36,7 +47,66 @@ client.on('interactionCreate', async (interaction) => {
   switch (interaction.data.name) {
     case 'orbify': {
       const interactionMessage = await interaction.reply({
-        content: 'Cramming your image into an orb...',
+        content: 'Downloading your image...',
+      });
+
+      const image = interaction.data.options.getAttachment('image')!;
+
+      await mkdir(`/tmp/orbs/${interaction.id}`, { recursive: true });
+
+      // Download the image to the local filesystem
+      const imageDownload = fetch(image.url);
+      const imageBuffer = await imageDownload.then((res) => res.bytes());
+      const filename = `/tmp/orbs/${interaction.id}/orb-input.${image.filename
+        .split('.')
+        .pop()}`;
+      await writeFile(filename, imageBuffer);
+
+      // Orbify the image
+      interactionMessage.message?.edit({
+        content: 'Orbifying your image with blender...',
+      });
+      const orbify = exec(
+        `blender -y -b blender-orbifier/sphere.blend -- --texture "${filename}" --output /tmp/orbs/${interaction.id}/orb.mp4`,
+      );
+
+      orbify.stdout?.on('data', (data) => {
+        console.log(data);
+      });
+
+      orbify.on('close', () => {
+        interactionMessage.message?.edit({
+          content: 'Converting your orb into a gif...',
+        });
+        const convert = exec(
+          `./blender-orbifier/gif-script.sh /tmp/orbs/${interaction.id}/orb.mp4 /tmp/${interaction.id}/orb`,
+        );
+
+        convert.stdout?.on('data', (data) => {
+          console.log(data);
+        });
+
+        convert.on('close', async () => {
+          interactionMessage.message?.edit({
+            content: 'Uploading your orb...',
+          });
+          const orb = await client.rest.channels.createMessage(
+            interaction.channelID,
+            {
+              content: 'Here is your orb!',
+              files: [
+                {
+                  name: 'orb.gif',
+                  contents: await readFile(
+                    `/tmp/orbs/${interaction.id}/orb-256x256.gif`,
+                  ),
+                },
+              ],
+            },
+          );
+
+          interactionMessage.message?.delete();
+        });
       });
 
       break;
